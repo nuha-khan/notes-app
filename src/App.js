c2dd4a63c9f6c2f535ad22072004af3df03cf1c9
nuha-khan <nuhakhan288@gmail.com>
2025-08-28 22:47:48 +0530

    Reorganized src folder

--- src/App.js
+++ src/App.js
@@ -1,1 +1,45 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import AddNote from "./AddNote";
 
+function App() {
+  const [notes,setNotes]=useState([]);
+
+  function handleClick(note){
+    setNotes(prevNote => {
+      return [...prevNote,note];
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <AddNote btnClick={handleClick}/>
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
